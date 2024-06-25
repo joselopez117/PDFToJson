ec9ee2139279db2d6b090661d8c04a2b0f50afa1
joselopez117 <74109741+joselopez117@users.noreply.github.com>
2024-06-24 21:02:12 -0500

    initial commit

---
+++ README.txt
@@ -0,0 +1,7 @@
+1. Download the project to your computer
+2. Copy and paste your PDF of choice into the files folder (remove the test pdf)
+3. Rename the pdf you want scanned to "filetoscan.pdf"
+4. Open the project and open a terminal (ctrl + `)
+5. Type "npm install" allow for commmand to run do not cancel it
+6. Type "node runthis.js"
+7. in the output folder, you should have the resulting json!
---
+++ runthis.js
@@ -0,0 +1,89 @@
+const { ServicePrincipalCredentials, PDFServices, MimeType, ExtractPDFParams, ExtractElementType, ExtractPDFJob, ExtractPDFResult } = require('@adobe/pdfservices-node-sdk');
+const fs = require('fs');
+const AdmZip = require('adm-zip');
+const path = require('path');
+
+const jsonFilePath = path.join(__dirname, 'pdfservices-api-credentials.json');
+
+let pdfServices;
+
+// Function to initialize PDF Services
+const initializePDFServices = () => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(jsonFilePath, 'utf8', (err, data) => {
+      if (err) {
+        return reject('Error reading the JSON file: ' + err);
+      }
+
+      try {
+        const jsonData = JSON.parse(data);
+        process.env.PDF_SERVICES_CLIENT_ID = jsonData.client_credentials.client_id;
+        process.env.PDF_SERVICES_CLIENT_SECRET = jsonData.client_credentials.client_secret;
+
+        const credentials = new ServicePrincipalCredentials({
+          clientId: process.env.PDF_SERVICES_CLIENT_ID,
+          clientSecret: process.env.PDF_SERVICES_CLIENT_SECRET
+        });
+        
+        pdfServices = new PDFServices({ credentials });
+        resolve();
+      } catch (err) {
+        reject('Error parsing the JSON data: ' + err);
+      }
+    });
+  });
+};
+
+const processPDF = async () => {
+  try {
+    await initializePDFServices();
+
+    const inputAsset = await pdfServices.upload({
+      readStream: fs.createReadStream('./files/filetoscan.pdf'),
+      mimeType: MimeType.PDF
+    });
+
+    const params = new ExtractPDFParams({
+      elementsToExtract: [ExtractElementType.TEXT]
+    });
+
+    const job = new ExtractPDFJob({ inputAsset, params });
+
+    const pollingURL = await pdfServices.submit({ job });
+    const pdfServicesResponse = await pdfServices.getJobResult({
+      pollingURL,
+      resultType: ExtractPDFResult
+    });
+
+    const resultAsset = pdfServicesResponse.result.resource;
+    const streamAsset = await pdfServices.getContent({ asset: resultAsset });
+
+    const outputFilePath = "./output/ExtractTextInfoFromPDF.zip";
+    console.log(`Saving asset at ${outputFilePath}`);
+
+    const writeStream = fs.createWriteStream(outputFilePath);
+    streamAsset.readStream.pipe(writeStream);
+
+    writeStream.on('finish', () => {
+        console.log('Extraction complete');
+        const zip = new AdmZip(outputFilePath);
+        const extractDir = path.join(__dirname, 'output');
+        zip.extractAllTo(extractDir, true);
+        console.log(`Files extracted to ${extractDir}`);
+  
+        // Read the extracted JSON file
+        const jsondata = fs.readFileSync(path.join(extractDir, 'structuredData.json'), 'utf8');
+        const data = JSON.parse(jsondata);
+  
+        data.elements.forEach(element => {
+          if (element.Path.endsWith('/H1')) {
+            console.log(element.Text);
+          }
+        });
+      });
+  } catch (error) {
+    console.error('Error processing PDF:', error);
+  }
+};
+
+processPDF();
